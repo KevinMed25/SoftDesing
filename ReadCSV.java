192036e218e59b96512516cb655b4da07790bf64
Alejandro Medina <kevinmedina2509@gmail.com>
2023-01-19 00:47:29 -0600

    disenio

--- ReadCSV.java
+++ ReadCSV.java
@@ -1,27 +1,51 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 // Clase para leer un archivo csv y almacenarlo en una matriz
+// Kevin Alejandro Medina Padilla
+// Diseño de Software 
 
 public class ReadCSV {
 
-    String nombre = null;
-    String apellidoMaterno = null;
-    String apellidoPaterno = null;
-    String matricula = null;
+    private static int cantidadAlumnos = 3; // Al ser un array estatico, se debe indicar la cantidad de alumnos en la lista
+    private static String alumnos[][] = new String[cantidadAlumnos][4]; 
+    
+    public static void leerCSV(){
+    
+        String line = "";
 
-    String[][][][] alumno = new String[matricula.length()][nombre.length()][apellidoPaterno.length()][apellidoMaterno
-            .length()];
+        try {
 
-    public static void readString() throws IOException {
+            FileReader reader = new FileReader("lista.csv");
+            BufferedReader read = new BufferedReader(reader);
 
-        File lista = new File("lista.csv");
+            for (int i = 0; i < cantidadAlumnos; i++){
+                line = read.readLine();
+                getRecords(line,i); 
+            }
+            
+        } catch (IOException e){
+            System.out.println("Error: " + e);
+        }
+    }
 
-        FileReader reader = new FileReader(lista);
-        BufferedReader read = new BufferedReader(reader);
+    public static String[][] getRecords(String line, int i){    
+  
+        String[] split = line.split(",");
 
+        for (int j = 0; j < split.length; j++){
+
+            alumnos[i][j] = split[j];
+            System.out.print(alumnos[i][j] + " ");
+                
+        }
+        System.out.println("");
+
+        return alumnos;
+    }
+
+    public static void main(String[] args) {
+        leerCSV();
     }
 
 }
