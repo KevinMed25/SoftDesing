e9a2c141c0ac79e182093b05cf29eae751e1e231
Kevin Medina <kevinmedina2509Jr@gmail.com>
2023-01-20 12:18:31 -0600

    merge

---
+++ V2/Alumno.java
@@ -0,0 +1,18 @@
+public class Alumno {
+
+    String Matricula;
+    String Nombre;
+    String ApellidoPaterno;
+    String ApellidoMaterno;
+    String Calificacion;
+
+    public Alumno(String Matricula, String Nombre, String ApellidoPaterno, String ApellidoMaterno,
+            String Calificacion) {
+        this.Matricula = Matricula;
+        this.Nombre = Nombre;
+        this.ApellidoPaterno = ApellidoPaterno;
+        this.ApellidoMaterno = ApellidoMaterno;
+        this.Calificacion = Calificacion;
+
+    }
+}
