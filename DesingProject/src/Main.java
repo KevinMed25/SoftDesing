38573267e7bb871fe6c44f550989081ce5dfb99c
Kevin Medina <kevinmedina2509Jr@gmail.com>
2023-02-02 12:06:47 -0600

    se agrega proyecto de DS

---
+++ DesingProject/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String path = "C:/Users/kevin/Desktop/SoftDesing/V2/lista.csv";
+        ReadCSV leer = new ReadCSV();
+
+        leer.leerCSV(path);
+        // leer.getRecords();
+    }
+
+}
